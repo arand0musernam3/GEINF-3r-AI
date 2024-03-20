f6c859a6a5204f6ea2b7b6a0aec5159f13f17f2c
Aniol Juanola <u1978893@campus.udg.edu>
2024-03-20 15:49:25 +0100

    Avançada la pràctica 3. Queda la part difícil :)

--- practic3/P3.py
+++ practic3/P3.py
@@ -1,53 +1,165 @@
 # NAME: Aniol Juanola Vilalta (u1978893)
-import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-from matplotlib import pyplot
 from sklearn.preprocessing import LabelEncoder
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
+# Though the following import is not directly being used, it is required
+# for 3D projection to work with matplotlib < 3.2
+import mpl_toolkits.mplot3d  # noqa: F401
 
 # Load the dataset in "steam.csv". We want to perform a clustering using k-means, scikit-
 # learn only implements Euclidean distance so only accepts numeric attributes, transform
 # all the attributes to numeric. Preprocess the data in other to convert categorical and non-
 # numeric attributes to numeric. (1 point)
 
+le_name = LabelEncoder()
+le_developer = LabelEncoder()
+le_publisher = LabelEncoder()
+le_categories = LabelEncoder()
+le_genres = LabelEncoder()
+le_owners = LabelEncoder()
+
+
 def read_model():
     df = pd.read_csv("steam.csv")
+    print("NaN values")
+    print("==========")
+    print(df.isna().any())  # Developer and Publisher have NaN values
+    print("==========")
+    print()
+    df['developer'] = df['developer'].fillna("THIS_IS_A_NAN_VALUE_REPLACE_ME")  # Placeholder for future reference when dealing with NaNs
+    df['publisher'] = df['publisher'].fillna("THIS_IS_A_NAN_VALUE_REPLACE_ME")
     df['release_date'] = pd.to_datetime(df['release_date']).astype('int64')
-    # appid, name, release_date, english, developer, publisher, required_age, categories, genres, achievements,
-    # positive_ratings, negative_ratings, average_playtime, median_playtime, owners, price
-    le_name = LabelEncoder()
-    le_developer = LabelEncoder()
-    le_publisher = LabelEncoder()
-    le_categories = LabelEncoder()
-    le_genres = LabelEncoder()
-    le_owners = LabelEncoder()
+    # converting the categorical variables into numerical
     df['name'] = le_name.fit_transform(df['name'])
     df['developer'] = le_developer.fit_transform(df['developer'])
     df['publisher'] = le_publisher.fit_transform(df['publisher'])
     df['categories'] = le_categories.fit_transform(df['categories'])
     df['genres'] = le_genres.fit_transform(df['genres'])
     df['owners'] = le_owners.fit_transform(df['owners'])
+    return df
 
 
+data = read_model()
 
-read_model()
 # Fill the NaN values in the dataset. Use A KNN algorithm. Was a good decision to fill these
 # values using KNN or is better to give them a fix value or just delete them? What will
 # happen in each case? (3 points)
 
 ## In order to fill the NaN values we need to use a KNN algorithm. The following code does that:
-model = KNeighborsClassifier()
+
+data['publisher'] = data['publisher'].replace(le_publisher.transform(["THIS_IS_A_NAN_VALUE_REPLACE_ME"])[0], np.nan)  # getting back the np.nan values
+data['developer'] = data['developer'].replace(le_developer.transform(["THIS_IS_A_NAN_VALUE_REPLACE_ME"])[0], np.nan)
+imputer = KNNImputer()
+dataProcessed = imputer.fit_transform(data)
+dataProcessed = pd.DataFrame(dataProcessed, columns=data.columns)
+print(dataProcessed)
+
+## TODO WRITE EXPLICACIÓ FINAL DE QUÈ EN PENSO: PROBABLEMENT HAURIA ESTAT MILLOR AFEGIR UN VALOR TIPU "UNKNOWN" I ALE :)
 
 # Clustering needs data to be scaled. If not variables with higher variability will be favoured.
 # Scale the data. (1 point)
+### Buscar com normalitzar les dades.
+
+
+
+def scaleColumns(df, cols_to_scale):
+    scaler = MinMaxScaler()
+    for col in cols_to_scale:
+        df[col] = pd.DataFrame(scaler.fit_transform(pd.DataFrame(df[col])),columns=[col])
+    return df
+
+
+scaleColumns(dataProcessed, dataProcessed.columns)
+print(dataProcessed)
 
 # Fit a k-means clustering with 2 clusters and perform the prediction. Retrieve the cluster
 # centers (cluster_centers_ attribute on your fitted k-means). Try to understand what kind
 # of information each cluster has. (2 points)
+### No hi ha una forma predefinida, fer plots i osties i intentar endivinar-ho.
+
+model = KMeans(n_clusters=3, init='k-means++')
+model.fit(dataProcessed)
+print(model.cluster_centers_)
+# https://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_iris.html#sphx-glr-auto-examples-cluster-plot-cluster-iris-py
+
 
 # Set yourself a clustering goal. Do you see any not useful variable? delete it. Try other
 # cluster numbers, preprocessing, variable selections, add weights add information in
 # other files... (do what you want to achieve a goal). Try to explain what are you expecting
 # to achieve, what are you doing and what you achieve. (3 points)
+
+### molaria fer un cluster de jocs que s'assemblen segons categories i osties jeje
+
+# Code source: Gaël Varoquaux
+# Modified for documentation by Jaques Grobler
+# License: BSD 3 clause
+
+import matplotlib.pyplot as plt
+
+# Though the following import is not directly being used, it is required
+# for 3D projection to work with matplotlib < 3.2
+import mpl_toolkits.mplot3d  # noqa: F401
+import numpy as np
+
+from sklearn import datasets
+from sklearn.cluster import KMeans
+
+np.random.seed(5)
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+estimators = [
+    ("k_means_iris_8", KMeans(n_clusters=8)),
+    ("k_means_iris_3", KMeans(n_clusters=3)),
+    ("k_means_iris_bad_init", KMeans(n_clusters=3, n_init=1, init="random")),
+]
+
+fig = plt.figure(figsize=(10, 8))
+titles = ["8 clusters", "3 clusters", "3 clusters, bad initialization"]
+for idx, ((name, est), title) in enumerate(zip(estimators, titles)):
+    ax = fig.add_subplot(2, 2, idx + 1, projection="3d", elev=48, azim=134)
+    est.fit(X)
+    labels = est.labels_
+
+    ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=labels.astype(float), edgecolor="k")
+
+    ax.xaxis.set_ticklabels([])
+    ax.yaxis.set_ticklabels([])
+    ax.zaxis.set_ticklabels([])
+    ax.set_xlabel("Petal width")
+    ax.set_ylabel("Sepal length")
+    ax.set_zlabel("Petal length")
+    ax.set_title(title)
+
+# Plot the ground truth
+ax = fig.add_subplot(2, 2, 4, projection="3d", elev=48, azim=134)
+
+for name, label in [("Setosa", 0), ("Versicolour", 1), ("Virginica", 2)]:
+    ax.text3D(
+        X[y == label, 3].mean(),
+        X[y == label, 0].mean(),
+        X[y == label, 2].mean() + 2,
+        name,
+        horizontalalignment="center",
+        bbox=dict(alpha=0.2, edgecolor="w", facecolor="w"),
+    )
+
+ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y, edgecolor="k")
+
+ax.xaxis.set_ticklabels([])
+ax.yaxis.set_ticklabels([])
+ax.zaxis.set_ticklabels([])
+ax.set_xlabel("Petal width")
+ax.set_ylabel("Sepal length")
+ax.set_zlabel("Petal length")
+ax.set_title("Ground Truth")
+
+plt.subplots_adjust(wspace=0.25, hspace=0.25)
+plt.show()
