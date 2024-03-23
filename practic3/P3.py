8a78f2379cc580bbad277317b3d850bd4a6baab5
Aniol Juanola <u1978893@campus.udg.edu>
2024-03-23 15:34:04 +0100

    No he fet gaire res, he buscat molt i em falta inspiració...

--- practic3/P3.py
+++ practic3/P3.py
@@ -1,6 +1,7 @@
 # NAME: Aniol Juanola Vilalta (u1978893)
 import numpy as np
 import pandas as pd
+from sklearn.metrics import silhouette_samples
 from sklearn.preprocessing import LabelEncoder
 from sklearn.impute import KNNImputer
 from sklearn.preprocessing import MinMaxScaler
@@ -8,10 +9,9 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d  # noqa: F401
 
-# Load the dataset in "steam.csv". We want to perform a clustering using k-means, scikit-
-# learn only implements Euclidean distance so only accepts numeric attributes, transform
-# all the attributes to numeric. Preprocess the data in other to convert categorical and non-
-# numeric attributes to numeric. (1 point)
+# Load the dataset in "steam.csv". We want to perform a clustering using k-means, scikit- learn only implements
+# Euclidean distance so only accepts numeric attributes, transform all the attributes to numeric. Preprocess the data
+# in other to convert categorical and non-numeric attributes to numeric. (1 point)
 
 le_name = LabelEncoder()
 le_developer = LabelEncoder()
@@ -28,7 +28,8 @@ def read_model():
     print(df.isna().any())  # Developer and Publisher have NaN values
     print("==========")
     print()
-    df['developer'] = df['developer'].fillna("THIS_IS_A_NAN_VALUE_REPLACE_ME")  # Placeholder for future reference when dealing with NaNs
+    df['developer'] = df['developer'].fillna(
+        "THIS_IS_A_NAN_VALUE_REPLACE_ME")  # Placeholder for future reference when dealing with NaNs
     df['publisher'] = df['publisher'].fillna("THIS_IS_A_NAN_VALUE_REPLACE_ME")
     df['release_date'] = pd.to_datetime(df['release_date']).astype('int64')
     # converting the categorical variables into numerical
@@ -49,41 +50,58 @@ data = read_model()
 
 ## In order to fill the NaN values we need to use a KNN algorithm. The following code does that:
 
-data['publisher'] = data['publisher'].replace(le_publisher.transform(["THIS_IS_A_NAN_VALUE_REPLACE_ME"])[0], np.nan)  # getting back the np.nan values
+data['publisher'] = data['publisher'].replace(le_publisher.transform(["THIS_IS_A_NAN_VALUE_REPLACE_ME"])[0],
+                                              np.nan)  # getting back the np.nan values
 data['developer'] = data['developer'].replace(le_developer.transform(["THIS_IS_A_NAN_VALUE_REPLACE_ME"])[0], np.nan)
 imputer = KNNImputer()
 dataProcessed = imputer.fit_transform(data)
 dataProcessed = pd.DataFrame(dataProcessed, columns=data.columns)
 print(dataProcessed)
 
-## TODO WRITE EXPLICACIÓ FINAL DE QUÈ EN PENSO: PROBABLEMENT HAURIA ESTAT MILLOR AFEGIR UN VALOR TIPU "UNKNOWN" I ALE :)
+
+## The generated values for the NaN instances don't make much sense, as "similar" games needn't have similar developers.
+## An example would be a shooter like Battlefield or Call of Duty, both similar in terms of gameplay but with
+## different publishers and developers. It would have made more sense to generate a new value "Unknown", which could
+## encapsulate the lack of knowledge without compromising the data with false associations.
+
+## It must be noted that the "Unknown" value also affects the games whose developers and publishers are NaN,
+## as they would have a "fake" similarity in this matter. Another option could have been to manually determine its
+## developer or, as a last resort, dropping the instances (which are not many) could still leave a valuable dataset to
+## work with.
 
 # Clustering needs data to be scaled. If not variables with higher variability will be favoured.
 # Scale the data. (1 point)
-### Buscar com normalitzar les dades.
 
 
-
-def scaleColumns(df, cols_to_scale):
+def scaleColumns(df):
     scaler = MinMaxScaler()
-    for col in cols_to_scale:
-        df[col] = pd.DataFrame(scaler.fit_transform(pd.DataFrame(df[col])),columns=[col])
-    return df
+    scaled_features = pd.DataFrame(scaler.fit_transform(df))
+    scaled_features.columns = df.columns
+    return scaled_features
 
 
-scaleColumns(dataProcessed, dataProcessed.columns)
+dataProcessed = scaleColumns(dataProcessed)
 print(dataProcessed)
 
 # Fit a k-means clustering with 2 clusters and perform the prediction. Retrieve the cluster
 # centers (cluster_centers_ attribute on your fitted k-means). Try to understand what kind
 # of information each cluster has. (2 points)
-### No hi ha una forma predefinida, fer plots i osties i intentar endivinar-ho.
 
+
+# TODO: mirar quin preprocessat més es pot fer, fer gràfics de resultats, etc. donar una explicació raonable
+# https://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_iris.html#sphx-glr-auto-examples-cluster-plot-cluster-iris-py
+# https://towardsdatascience.com/k-means-clustering-algorithm-applications-evaluation-methods-and-drawbacks-aa03e644b48a
+
+
+# =====================================================================================================
 model = KMeans(n_clusters=2, init='k-means++')
 model.fit(dataProcessed)
-print(model.cluster_centers_)
-#TODO: mirar quin preprocessat més es pot fer, fer gràfics de resultats, etc. donar una explicació raonable
-# https://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_iris.html#sphx-glr-auto-examples-cluster-plot-cluster-iris-py
+label = model.predict(dataProcessed)
+u_labels = np.unique(label)
+
+
+
+# =====================================================================================================
 
 
 # Set yourself a clustering goal. Do you see any not useful variable? delete it. Try other
